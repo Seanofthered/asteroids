dededf1ce0574e7dc36a8afd64009ee7edbb081c
Seanofthered <s_ellis@outlook.com>
2025-06-09 13:57:37 -0500

    player can shoot

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 from player import Player
 from asteroid import *
 from asteroidfield import *
+from shot import Shot
+import sys
 
 def main():
     pygame.init()
@@ -17,14 +19,16 @@ def main():
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (drawable, updateable)
     Asteroid.containers = (asteroids, updateable, drawable)
     AsteroidField.containers = (updateable)
-
+    Shot.containers = (shots, updateable, drawable)
 
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT/2), PLAYER_RADIUS)
     field = AsteroidField()
-    
+
 
     while True:
         for event in pygame.event.get():
@@ -39,6 +43,10 @@ def main():
 
         updateable.update(dt)
 
+        for asteroid in asteroids:
+            if player.collision_check(asteroid):
+                print("Game over!")
+                sys.exit()
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
