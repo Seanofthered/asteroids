2b8e2973160f89747c13bf4cae109d6dde64e267
Seanofthered <s_ellis@outlook.com>
2025-06-09 14:14:40 -0500

    added countdown timer to shots fired

--- player.py
+++ player.py
@@ -8,7 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, radius)
         
         self.rotation = 0
-
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +27,8 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        self.timer = max(self.timer -dt, 0)
+
         if keys[pygame.K_a]:
             self.rotate(-1 * dt)
         if keys[pygame.K_d]:
@@ -36,7 +38,11 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-1 * dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer > 0:
+                pass
+            else:
+                self.shoot()
+                self.timer = 0.3
 
 
     def move(self, dt):
